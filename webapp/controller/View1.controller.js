9c6c414776cdcd27147ae9dbf3fe790c32e2c601
santhosh0221 <santhoshkumar.c@signiwis.com>
2020-08-11 13:24:01 +0000

    1st commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("toDelete.toDelete.controller.View1", {
+		onInit: function () {
+
+		},
+		message: function () {
+			MessageToast.show("Hai");
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"toDelete/toDelete/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
